f329324ef12c007030259b6fcb74fea35130afcd
alana-cruz <alana.cruz3@upr.edu>
2026-03-12 08:28:55 -0400

    Added DyEnemy Assets

--- src/Entities/Enemies/DyEnemy.hpp
+++ src/Entities/Enemies/DyEnemy.hpp
@@ -6,13 +6,21 @@ class DyEnemy : public Enemy {
         float angle = 135;
         float aimAngle = 225;
         bool loop = false;
+        Rectangle rectTexture;
 
     public:
         DyEnemy(float x, float y) : Enemy(x, y) { 
             this->cooldown = GetRandomValue(90, 300);
             this->health = 1; 
-        }
 
+            if(GetRandomValue(0,1) == 0){
+                this->rectTexture = Rectangle{2, 128, 13, 14};
+            }
+            else{
+                this->rectTexture = Rectangle{2, 147, 13, 13};
+            }
+        }
+        
         void draw() override;
         void update(std::pair<float, float> pos, HitBox target) override;
         void attack(HitBox target) override;
